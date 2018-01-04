892728383840f4ae90566360eeeaf18ec0449d5c
Dmitry Kalenyuk <dmitrykalenyuk@gmail.com>
2018-01-04 13:50:37 +0200

    pretty render method

--- src/Template.php
+++ src/Template.php
@@ -99,64 +99,7 @@ class Template
      *
      * also this method implements a custom syntax using regular expressions
      */
-    public function __render($file)
-    {
-        $path = __DIR__ . '/../templates/' . $file . '.php';
-        if (file_exists($path)) {
-            $content = file_get_contents($path);
 
-            $content = preg_replace_callback("/{% partial '(.*)' %}/", function ($matches) {
-                return file_get_contents(__DIR__ . '/../templates/' . $matches[1] . '.php');
-            }, $content);
-
-            $content = preg_replace_callback("/{% partial '(.*)', (\[.*\]) %}/", function ($matches) {
-                return "<?php extract(" . $matches[2] . "); ?>\n" . file_get_contents(__DIR__ . '/../templates/' . $matches[1] . '.php');
-            }, $content);
-
-            $content = preg_replace('/{% (.*?) %}/', '<?= \$$1 ?>', $content);
-            //custom syntax for foreach cycle
-            $content = preg_replace('/\{% foreach (.*) %}/', '<?php foreach ($1) : ?>', $content);
-            $content = preg_replace('/\{% endforeach %}/', '<?php foreach; ?>', $content);
-
-            //custom syntax for if condition
-            $content = preg_replace('/\{% if (.*) %}/', '<?php if ($1) : ?>', $content);
-            $content = preg_replace('/\{% else %}/', '<?php else : ?>', $content);
-            $content = preg_replace('/\{% endif %}/', '<?php endif; ?>', $content);
-
-            //custom syntax for php tags
-            //TODO:: Similar syntax for all constructions!!!
-            $content = preg_replace('/\{ (.*) }/', '<?php $this->$1 ?>', $content);
-
-            //to see the compiled php code
-            $compiled_path = __DIR__ . '/../compiled_templates/' . $file . '.php';
-            file_put_contents($compiled_path, $content);
-
-            ob_start();
-            extract($this->vars);
-            require_once __DIR__ . "/../templates/" .  $this->layout .".php";
-            $layout = ob_get_clean();
-
-            eval(' ?>' . $layout . $content . '<?php ');
-
-            return ob_get_clean();
-
-        } else {
-            exit('<h1>Template Error</h1>');
-        }
-    }
-
-    public function render($file)
-    {
-        $path = __DIR__ . '/../templates/' . $file . '.php';
-        $content = file_get_contents($path);
-        $content = preg_replace('/{% (.*?) %}/', '<?= \$$1 ?>', $content);
-        ob_start();
-        extract($this->vars);
-        require_once __DIR__ . "/../templates/" . $this->layout . ".php";
-        $layout = ob_get_clean();
-        eval(' ?>' . $layout . $content . '<?php ');
-        return ob_get_clean();
-    }
 
     public function partial($file, array $vars = [])
     {
@@ -170,6 +113,16 @@ class Template
         extract($this->vars);
         eval(' ?>' . $content . '<?php ');
         return ob_get_clean();
+    }
+
+    public  function render($file)
+    {
+        ob_start();
+        extract($this->vars);
+        require_once __DIR__ . "/../templates/" . $this->layout . ".php";
+        require_once __DIR__ . "/../templates/$file.php";
+        $output = ob_get_clean();
+        return $output;
     }
 
 }
